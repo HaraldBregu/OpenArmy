11e8079f9f7ff9ceb7db39c0d4db259dca1b7c71
Harald Bregu <harald.bregu@gmail.com>
2026-05-20 09:33:19 +0200

    auto: commit after tool 2026-05-20 09:33:19

--- src/constants.ts
+++
@@ -1,6 +0,0 @@
-import path from "path";
-import os from "os";
-
-export const OA_HOME = path.join(os.homedir(), ".oa");
-export const OA_PACKAGES_DIR = path.join(OA_HOME, "packages");
-export const OA_REGISTRY_PATH = path.join(OA_HOME, "registry.json");
